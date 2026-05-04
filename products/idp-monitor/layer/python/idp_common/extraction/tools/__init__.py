256a117c54b4daa0d08ae7fd29c431dbaec9fe5b
Mansoor Malik <webarch@gmail.com>
2026-05-04 10:32:45 -0400

    chore: stop tracking layer build artifacts (products/idp-monitor/layer/python/)

--- products/idp-monitor/layer/python/idp_common/extraction/tools/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-
-"""Extraction tools for agentic extraction."""
-
-from idp_common.extraction.tools.table_parser import create_parse_table_tool
-
-__all__ = ["create_parse_table_tool"]
