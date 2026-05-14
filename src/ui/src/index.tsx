52db3bb2e9145d9e5cad6476261afa96552895b2
Taniya Mathur <tanimath@amazon.com>
2026-05-14 12:51:51 +0000

    fixing lint

--- src/ui/src/index.tsx
+++ src/ui/src/index.tsx
@@ -49,7 +49,6 @@ import CloudscapeButtonDropdown from '@cloudscape-design/components/button-dropd
 import CloudscapeCheckbox from '@cloudscape-design/components/checkbox';
 import CloudscapeProgressBar from '@cloudscape-design/components/progress-bar';
 import CloudscapePieChart from '@cloudscape-design/components/pie-chart';
-import CloudscapeTabs from '@cloudscape-design/components/tabs';
 import CloudscapeTextarea from '@cloudscape-design/components/textarea';
 import * as CloudscapeCollectionHooks from '@cloudscape-design/collection-hooks';
 
@@ -85,7 +84,6 @@ import * as CloudscapeCollectionHooks from '@cloudscape-design/collection-hooks'
   CloudscapeCheckbox,
   CloudscapeProgressBar,
   CloudscapePieChart,
-  CloudscapeTabs,
   CloudscapeTextarea,
   CloudscapeCollectionHooks,
 };
